928be457be783a8e0993349313c15b53a66627eb
無痕剑 <mayh0991@126.com>
2021-01-13 08:41:44 +0800

    修改配置依赖

--- gateway/src/test/java/com/wuhenjian/microservicescaffolding/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/wuhenjian/microservicescaffolding/gateway/GatewayApplicationTests.java
@@ -1,13 +1,5 @@
 package com.wuhenjian.microservicescaffolding.gateway;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class GatewayApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
